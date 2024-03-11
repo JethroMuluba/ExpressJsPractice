f338f78bdc91c1ea129aea4ca22c08ddec5bdedf
JethroMuluba <jethro.muluba@gmail.com>
2024-03-11 21:00:01 +0100

    comment each function

--- app.js
+++ app.js
@@ -2,27 +2,29 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const portMessage = "This app run on the port:";
-const clientMessage = 'Welcome To My Trying App';
 const errorMessage = 'Error during server creating';
 const runStatus = 200;
 const errorStatus = 404;
 const redirectedStatus = 300;
 
-
+//get home
 app.get('/home', (req, res, next) => {
     res.status(`${runStatus}`).sendFile('/html/home.html', { root: __dirname});   
 });
 
+//get about-me
 app.get('/about-me', (req, res) => {
     res.status(`${runStatus}`).sendFile('/html/aboutMe.html', { root: __dirname});   
 
 });
 
+
+//redirected user when he want to access on the root
 app.get('/', (req, res) => {
     res.status(`${redirectedStatus}`).redirect('/home');   
 });
 
-
+//creat Middleware to serve when user try to enter a wrong url
 app.use((req, res) => {
     res.status(`${errorStatus}`).sendFile('/html/error.html', { root: __dirname});   
 })
