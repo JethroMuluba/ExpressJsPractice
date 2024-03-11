73e5580a9af0026c5d3b45160872f527e37410ab
JethroMuluba <jethro.muluba@gmail.com>
2024-03-11 20:05:08 +0100

    Get method return Html File

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const port = 3000;
 const portMessage = "This app run on the port:";
 const clientMessage = 'Welcome To My Trying App';
 const errorMessage = 'Error during server creating';
+const runStatus = 200;
 
 
 app.get('/', (req, res, next) => {
-    res.send(`${clientMessage}`)    
+    res.status(`${runStatus}`).sendFile('/html/home.html', { root: __dirname})    
 })
 
 app.listen(port, () => {
